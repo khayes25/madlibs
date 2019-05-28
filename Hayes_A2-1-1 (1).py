2fdc8e95b92b638f1c7f12d6aaec75d10293a9b4
khayes25 <khayes25@student.ccp.edu>
2019-05-28 16:13:33 -0400

    first commit

---
+++ Hayes_A2-1-1 (1).py
@@ -0,0 +1,125 @@
+"""
+See instructions in CSCI111_A2.pdf
+
+Be sure to rename this file Lastname_A2.py, using your last name
+in place of "Lastname".
+
+Your name: Keon Hayes
+
+"""
+
+"""
+The input function will print "What is your celebrity name?: " and
+wait for the user to type their entry. When the user presses the
+"Enter" key on their keyboard, the value they typed will be
+returned and assigned to the celeb_name variable.
+"""
+
+celeb_name = input("What is your celebrity name?: ")
+
+"""
+The input function will print "Name a prestigious, expensive
+university: " and wait for the user to type their entry. When the user
+presses the "Enter" key on their keyboard, the value typed will be
+returned and assigned to the college variable.
+"""
+
+college = input("Name a prestigious, expensive university: ")
+
+"""
+The input function will print "What is your dream workplace?: "
+and wait for the user to type their entry. When the user presses the
+"Enter" key on their keyboard, the value they typed will be returned and
+assigned to the company variable.
+"""
+
+company = input("What is your dream workplace?: ")
+
+"""
+The input function will print "What is your dream job?: "
+and wait for the user to type their entry. When the user presses the
+"Enter" key on their keyboard, the value they typed will be returned
+and assigned to the job variable.
+"""
+
+job = input("What is your dream job?: ")
+
+"""
+The input function will print "Name an exotic, faraway
+place: " and wait for the user to type their
+entry. When the user presses the "Enter" key on their keyboard, the
+value they typed will be returned and assigned to the foreign_place
+variable.
+"""
+
+foreign_place = input("Name an exotic, faraway place: ")
+
+"""
+The input function will print "Name an expensive
+restaurant: " and wait for the user to type their entry. When the user
+presses the "Enter" key on their keyboard, the value they typed will be
+returned and assigned to the eat_out variable.
+"""
+
+eat_out = input("Name an expensive restaurant: ")
+
+"""
+The input function will print "Name a fun activity you like to do
+in the evening ^_^ (ending in -ing): " and wait for the user to type their entry. When
+the user presses the "Enter" key on their keyboard, the value they
+typed will be returned and assigned to the fun1 variable.
+"""
+
+fun1 = input("Name a fun activity you like to do in the evening ^_^ (ending in -ing): ") #The input needs to end in -ing so the sentence will be grammatically correct.
+
+"""
+The input function will print "Name another activity you like to do at
+night ^_^ (ending in -ing): " and wait for the user to type their entry. When the user
+presses the "Enter" key on their keyboard, the value they typed will be
+returned and assigned to the fun2 variable.
+"""
+
+fun2 = input("Name another activity you like to do at night ^_^ (ending in -ing): ")    #The input needs to end in -ing so the sentence will be grammatically correct.
+
+"""
+The input function will print "What would be a cool pet that no one
+has?: " and wait for the user to type their entry. When the user
+presses the "Enter" key on their keyboard, the value they typed will
+be returned and assigned to the pet variable.
+"""
+
+pet = input("What would be a cool pet that no one has?: ")
+
+"""
+The input function will print "Give your pet a unique name: "
+and wait for the user to type their entry. When the user presses the
+"Enter" key on their keyboard, the value they typed will be returned
+and assigned to the pet_name variable.
+"""
+
+pet_name = input("Give your pet a unique name: ")
+
+"""
+The input function will print "Name an expensive,
+luxury store: " and wait for the user to type their entry. When the
+user presses the "Enter" key on their keyboard, the value they typed
+will be returned and assigned to the store variable.
+"""
+
+store = input("Name an expensive, luxury store: ")
+
+"""
+Print a story to the user user using the values of the celeb_name,
+college, company, job, foreign_place, eat_out, fun1, fun2, pet,
+pet_name, and store variables.
+"""
+
+print("You can't escape it! The gaudy, extravagant life of", celeb_name, "is plastered all over social media.")
+print("Even though they allegedly never went to class,", celeb_name, "has a degree from", college, "based on their merits.")
+print("They totally got their job at", company, "based solely on these merits.")
+print(celeb_name, "is just like you! A", job, "doesn't make as much as you would think, but luckily their salary is a drop in the bucket compared to their offshore bank accounts!")
+print("By day, they are at", company, "toiling away. By night, they're on their private jet off to", foreign_place, "within minutes.")
+print("Their first stop was", eat_out, "to get something to eat. Pictures of them", fun1, "and", fun2, "can be found on Instagram.")
+print("Before flying home,", celeb_name, "bought a", pet, "named", pet_name, "to take home.")
+print(celeb_name, "arrives back at their spacious 9 bedroom home, while", pet_name, "has a new guest house in the back to roam around.")
+print(celeb_name, "and", pet_name, "lived happily ever after.")
